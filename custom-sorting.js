6615aa4552d0d1c403fb7cf0d39b86ec9e003f70
Gioanna Rheumer <ratofkryll@gmail.com>
2018-11-29 20:31:59 +0000

    sorted by age and name

---
+++ custom-sorting.js
@@ -0,0 +1,26 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+// Sorts decending by age
+students.sort(function(a, b) {
+  return b.age - a.age;
+});
+
+// Sorts age-sorted array alphabetically
+students.sort(function(a, b) {
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  return 0;
+});
+
+console.log(students);
